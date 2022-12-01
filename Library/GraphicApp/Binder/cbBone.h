9d03fd2869045612215fb6495bc01de078570b74
MuroyaIan <allay908028522@gmail.com>
2022-12-01 09:56:15 +0900

    バインダ改善

--- Library/GraphicApp/Binder/cbBone.h
+++ Library/GraphicApp/Binder/cbBone.h
@@ -40,13 +40,13 @@ public:
 	//プロトタイプ宣言
 	explicit CB_BONE(const GRAPHIC& Gfx, const CBD_BONE& aMtxBone, bool Transpose = false);
 	~CB_BONE() noexcept override;
-	void Bind(const GRAPHIC& Gfx) const noexcept override;			//バインド処理
+	void Bind(const GRAPHIC& Gfx) const noexcept override;		//バインド処理
 
 private:
 
 	//変数宣言
 	static std::unique_ptr<VS_CBUFFER<CBD_BONE>> m_pCBuffVS;	//定数バッファのポインタ
-	static int m_RefCount;											//定数バッファの利用数
+	static int m_RefCount;										//定数バッファの利用数
 	const CBD_BONE& m_aMtxBone;									//骨情報の提供先
-	bool m_bTransposeMatrix;										//転置処理実行フラグ
+	bool m_bTransposeMatrix;									//転置処理実行フラグ
 };
